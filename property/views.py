14cacc4754147fce4b29f9596b8fe249d30dc773
Ari Þórðarson <arith24@ru.is>
2025-05-06 15:12:37 +0000

    bætti við images og fokkaði í style sheet

--- property/views.py
+++ property/views.py
@@ -5,15 +5,17 @@ properties = [
     {
         "id": 1,
         "name": "Ari's house",
-        "image":
+        "image": "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg"
     },
     {
         "id": 2,
         "name": "Muggur's house",
+        "image": "https://hips.hearstapps.com/hmg-prod/images/west-virginia-gray-cottage-64dd6bb056057.jpg?crop=0.943xw:0.817xh;0.0224xw,0.0932xh&resize=980:*"
     },
     {
         "id": 3,
         "name": "Nói's house",
+        "image": "https://www.livehome3d.com/assets/img/articles/design-house/how-to-design-a-house.jpg"
     }
 ]
 
