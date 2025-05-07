15a22cabbf30e3dfeeaf47f4193ef26298563a2a
Valgarður Nói <Valgardurnoi@hotmail.com>
2025-05-07 16:46:59 +0000

    vinna með user og login

--- User/urls.py
+++ User/urls.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from User import views
+from . import views
     
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(template_name='user/login.html'), name='logout'),
     path('register', views.register, name='register'),
+
+    path('profile', views.profile, name='profile'),
 ]
--- User/views.py
+++ User/views.py
@@ -5,15 +5,15 @@ from django.contrib.auth import login
 
 def register(request):
     if request.method == 'POST':
-        print(1)
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])  # hash password
-            user.save()
+            form.save()
             login(request, user)  # log them in after registering
-            return redirect('home')  # or redirect to dashboard
+            return redirect('login')  # or redirect to dashboard
     else:
         form = UserRegistrationForm()
     return render(request, 'User/register.html', {
             'form': form})
+
+def profile(request):
+    return render(request, 'user/profile.html')
